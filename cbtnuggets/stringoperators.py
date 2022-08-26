70a9e19d72cdecabf2586ced56e352b618835e86
mountaindrewtech <dschmidtawe@gmail.com>
2022-08-26 17:00:38 -0400

    update

---
+++ cbtnuggets/stringoperators.py
@@ -0,0 +1,14 @@
+name1 = "Ben"
+name2 = "Finkel"
+
+fullName = name1 + " " + name2
+
+print(fullName)
+
+textValue = "RepeatME"
+
+#textValue = textValue * 5
+#textValue = textValue[3]
+textValue = textValue[3:5]
+
+print(textValue)
